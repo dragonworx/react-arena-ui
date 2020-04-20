d9cc02b7df3bb523a6c1aa0f7f5a3fe2f9ccbd3a
Ali Chamas <achamas@atlassian.com>
2020-04-20 22:54:30 +1000

    affineview success

--- public/app.tsx
+++ public/app.tsx
@@ -1,69 +1,18 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { AffineView } from './affineview';
 import './app.less';
 
 export function App() {
-   // S=Z*C+W/2+P
-   
-   /*
-   c=
-   −2p+2s−w
-   /
-   2z
-   */
-
-   const [ info, setInfo ] = useState('');
-   const [ zoom, setZoom ] = useState(1);
-
-   const screenW = 200;
-   const screenH = 200;
-
-   const panX = 20;
-   const panY = 50;
-   const boxW = 30;
-   const boxH = 30;
-
-   const cartToScreen = (x: number, y: number) => {
-      const sx = (zoom * x) + (screenW / 2) + panX;
-      const sy = (zoom * y) + (screenH / 2) + panY;
-      return {x: sx, y: sy};
-   }
-
-   const screenToCart = (x: number, y: number) => {
-      const cx = ((-2 * panX) + (2 * x) - screenW) / (2 * zoom);
-      const cy = ((-2 * panY) + (2 * y) - screenH) / (2 * zoom);
-      return { x: cx, y: cy };
-   };
-
-   const { x: left, y: top } = cartToScreen(0, 0);
-   const width = boxW * zoom;
-   const height = boxH * zoom;
-   const style = { left, top, width, height, marginLeft: width * -0.5, marginTop: height * -0.5 };
-   const p = cartToScreen(0, 0);
-   console.log(p);
-
-   const onMouseMove = (e: React.MouseEvent) => {
-      const { clientX, clientY } = e;
-      const { x, y } = screenToCart(clientX, clientY);
-      setInfo(`zoom: ${zoom} cart: ${Math.round(x)}, ${Math.round(y)}`);
-   };
-
-   const onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
-      const { deltaY, altKey } = e;
-      const delta = deltaY > 0 ? 1 : -1;
-      const newZoom = zoom - delta;
-      setZoom(newZoom);
-   };
 
    return (
-      <div
-         className="canvas"
-         onMouseMove={onMouseMove}
-         onWheel={onWheel}
+      <AffineView
+         width={500}
+         height={500}
       >
-         {info}
-            <div className="box" style={style}></div>
-      </div>
+            <div className="box" style={{width: 100, height: 100}}></div>
+            <div className="box" style={{left: 100, top: 100, width: 200, height: 100}}></div>
+            <div className="box" style={{left: 300, top: 400, width: 200, height: 100}}></div>
+      </AffineView>
    )
 
 }
